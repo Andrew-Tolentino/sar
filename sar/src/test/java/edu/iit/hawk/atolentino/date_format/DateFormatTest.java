eebe9a765f239928ce4d495a09b49d95e958fc97
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2020-04-28 05:30:39 -0500

    Updated time zone bug

--- sar/src/test/java/edu/iit/hawk/atolentino/date_format/DateFormatTest.java
+++ sar/src/test/java/edu/iit/hawk/atolentino/date_format/DateFormatTest.java
@@ -7,16 +7,21 @@ import edu.iit.hawk.atolentino.response_generator.boundaries.DateFormatter;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Calendar;
+import java.util.TimeZone;
+
 
 public class DateFormatTest {
 	private static final long TIME_STAMP = 1586122589262L;
 	private static final String DEFAULT_FORMAT = "05-Apr-2020, 16:36:29";
 	private static final String TIME_FORMAT = "16:36";
 	private static final String DAY_MONTH_YEAR_FORMAT = "05-Apr-2020";
+	private TimeZone timeZone = TimeZone.getTimeZone("America/Chicago");
+
 	
 	@Test
 	void testDateFormatReturnsExpectedDefaultFormatOutput() {
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(timeZone);
 		c.setTimeInMillis(TIME_STAMP);
 		assertEquals(DEFAULT_FORMAT, DateFormatter.getDefaultFormat(c));
 	}
@@ -24,6 +29,7 @@ public class DateFormatTest {
 	@Test
 	void testDateFormatReturnsExpectedTimeFormatOutput() {
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(timeZone);
 		c.setTimeInMillis(TIME_STAMP);
 		assertEquals(TIME_FORMAT, DateFormatter.getTimeFormat(c));		
 	}
@@ -31,6 +37,7 @@ public class DateFormatTest {
 	@Test
 	void testDateFormatReturnsExpectedDayMonthYearFormatOutput() {
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(timeZone);
 		c.setTimeInMillis(TIME_STAMP);
 		assertEquals(DAY_MONTH_YEAR_FORMAT, DateFormatter.getDayMonthYearFormat(c));		
 	}
